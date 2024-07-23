6200c5d47a969560b441cb289b81b25946472265
Finley <finley.mwachia12@gmail.com>
2024-07-23 13:23:21 +0300

    done with the header section

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,11 +1,11 @@
 import React from "react";
-
+import './Header.css'
 const Header = () => {
   return (
     <section className="h-wrapper">
-      <div className="h-container">
+      <div className="flexCenter paddings innerWidth h-container">
         <img src="./logo.png" alt="logo" width={100} />
-        <div className="h-menu">
+        <div className="flexCenter h-menu">
           <a href="">Residencies</a>
           <a href="">Our Values</a>
           <a href="">Contact Us</a>
