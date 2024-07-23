62ac4210d90e6ae9c48ad4861939519e61f71226
Finley <finley.mwachia12@gmail.com>
2024-07-23 17:20:36 +0300

    add count up section

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,22 +1,36 @@
 import React from "react";
 import "./Hero.css";
-
+import { HiLocationMarker } from "react-icons/hi";
+import CountUp from "react-countup";
 const Hero = () => {
   return (
     <section className="hero-wrapper">
       <div className="paddings innerWidth flexCenter hero-container">
-        <div className="hero-left">
+        <div className="flexColStart hero-left">
           <div className="hero-title">
+            <div className="orange-circle" />
             <h1>
-              Discover <br />Most Suitable <br /> Property
+              Discover <br />
+              Most Suitable <br /> Property
             </h1>
           </div>
-          <div className="hero-des">
+          <div className="flexColStart hero-des">
             <span>Find a Variety of properties that suit you very easily</span>
-            <span>Forget all the difficulties in finding a residence for you</span>
+            <span>
+              Forget all the difficulties in finding a residence for you
+            </span>
           </div>
-          <div className="search-bar">
-            Search bar
+          <div className="flexCenter search-bar">
+            <HiLocationMarker color="var(--blue)" size={25} />
+            <input type="text" />
+            <button className="button">Search</button>
+          </div>
+
+          <div className="flexCenter stats">
+            <div className="flexColStart stat">
+              <span><CountUp start={8800}end={9000}duration={4} /> </span>
+              <span>Premium Products</span>
+            </div>
           </div>
         </div>
 
