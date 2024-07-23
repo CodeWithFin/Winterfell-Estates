2d6137291124715cfb59c7653117f5b41e4a4a8d
Finley <finley.mwachia12@gmail.com>
2024-07-23 16:44:39 +0300

    redore entire hero section

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -5,11 +5,24 @@ const Hero = () => {
   return (
     <section className="hero-wrapper">
       <div className="paddings innerWidth flexCenter hero-container">
-        <div className="hero-left">left section</div>
-        
+        <div className="hero-left">
+          <div className="hero-title">
+            <h1>
+              Discover <br />Most Suitable <br /> Property
+            </h1>
+          </div>
+          <div className="hero-des">
+            <span>Find a Variety of properties that suit you very easily</span>
+            <span>Forget all the difficulties in finding a residence for you</span>
+          </div>
+          <div className="search-bar">
+            Search bar
+          </div>
+        </div>
+
         <div className="flexCenter hero-right">
           <div className="image-container">
-            <img src="./hero-image.png" alt="" />
+            <img src="./hero-image.png" alt="Hero" />
           </div>
         </div>
       </div>
