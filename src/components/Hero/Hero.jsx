4bbeb17e873ccc74b19dec25e3195e3de7eb084c
Finley <finley.mwachia12@gmail.com>
2024-07-23 14:28:16 +0300

    create two section on hero section

---
+++ src/components/Hero/Hero.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const Hero = () => {
+  return (
+   <section className="hero-wrapper">
+    <div className="paddings innerWidth flexCenter hero-container">
+        <div className="hero-left">
+            left section
+        </div>
+        <div className="flexCenter hero-right">
+<div className="image-container">
+    <img src="./hero-image.png" alt="" />
+</div>
+        </div>
+        </div>  
+   </section>
+  )
+}
+
+export default Hero
